103d10a6ed7d32cdfe356c6292252f173216592f
Arnold-MV <arnold.vilela.98@gmail.com>
2023-06-07 18:40:20 -0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="z-50 flex items-center justify-center row-start-7 py-3 pb-20 mb-0 lg:fixed lg:bottom-0 lg:left-0 lg:mb-2 lg:w-60 bg-purple-950 lg:bg-transparent">
+    <footer className="z-50 flex items-center justify-center row-start-7 py-3 pb-20 mb-0 lg:fixed lg:bottom-0 lg:left-0 lg:-mb-10 lg:w-60 bg-purple-950 lg:bg-transparent ">
       <h3 className="font-normal opacity-70">&#169; Arnold Martinez villela</h3>
     </footer>
   );
